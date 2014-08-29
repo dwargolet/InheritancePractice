7531dd53ee675ae891a55d706b32d89a15592791
Daniel <Daniel@192.168.0.18>
2014-08-28 19:19:24 -0500

    Start of project, three required packages already created

---
+++ src/inheritancepractice/InheritancePractice.java
@@ -0,0 +1,13 @@
+package inheritancepractice;
+
+/**
+ *
+ * @author Daniel
+ */
+public class InheritancePractice {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
